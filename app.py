d7bc6e8f20937e6bf552833d6826166131b33712
Ayush Kumar <ayushkumarlp57707@gmail.com>
2026-06-08 14:51:01 +0530

    Add comment for why not use header

--- app.py
+++ app.py
@@ -44,7 +44,7 @@ def recommend(movie):
 
 
 # ------------------ Streamlit UI ------------------ #
-# st.header('🎬 Movie Recommender System')
+# st.header('🎬 Movie Recommender System')  That is not used beacause we can not apply style on it.
 st.markdown(
     "<h1 style='color:red; text-align:center;'>🎬 Movie Recommender System</h1>",
     unsafe_allow_html=True
